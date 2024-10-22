91b399fdaae001ea0cde4596607cb2f5d36b923e
Nalahatra <fitiavananalahatra@gmail.com>
2024-10-22 06:11:39 +0300

    feat:  create the navbar ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,13 @@ module.exports = {
       yellow: "#F9C66E",
       grey: "#D9D9D9",
       black: "#000000",
+      "soft-grey": "#A6A3A3",
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: "0px 2px 4px 0px #00000026",
+      },
+    },
   },
   plugins: [],
 };
