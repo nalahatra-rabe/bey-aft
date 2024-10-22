c3cd07f06aace6cccaf8d2baa29eaf8c58c8d501
Nalahatra <fitiavananalahatra@gmail.com>
2024-10-22 05:21:15 +0300

    refactor: change project structure  for tailwinds support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
+    colors: {
+      green: "#027580",
+      white: "#ffffff",
+      red: "#EE7563",
+      "white-smoke": "#F5F5F5",
+      "light-green": "#F0FBF7",
+      yellow: "#F9C66E",
+      grey: "#D9D9D9",
+      black: "#000000",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
